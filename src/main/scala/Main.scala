33e1d9499b96715a6d0d559b2de2ee899758e32e
chpache <christophe.pache@romande-energie.ch>
2018-10-17 09:59:49 +0200

    adds generic way to change node class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -23,6 +23,15 @@ object Main {
       createGrape("", 30, i => if (i % 2 == 0) "online" else "offline", randomColor)
     }
 
+    js.timers.setTimeout(it.next()) {
+      for (i <- 1 to 4) {
+        for (j <- 1 to 3) {
+          graph.addNode(NodeD3(s"new-$i-$j", s"the new Node $i$j", i, s"<p>tooltip $i;$j</p>", s"node-$i-$j", if (i % 2 == 0) "online" else "offline", shape(i)))
+          graph.addLink(LinkD3Json(s"new-$i", s"new-$i-$j", randomColor(j), width(j), endArrow = true))
+        }
+      }
+    }
+
     // Create a new grape with another name
     js.timers.setTimeout(it.next()) {
       createGrape(",1", 45, i => if (i % 3 == 0) "online" else "offline")
@@ -47,6 +56,12 @@ object Main {
       graph.removeAllLinks()
     }
 
+    js.timers.setTimeout(it.next()) {
+      graph.markNode("rootNode")(d => true)
+      graph.markNode("offline")(d => d.id contains "newroot")
+      graph.update()
+    }
+
     // Remove all the nodes
     js.timers.setTimeout(it.next()) {
       graph.removeAllNodes()
@@ -57,10 +72,21 @@ object Main {
       createGrape("--", 14, i => if (i % 4 == 0) "online" else "offline")
     }
 
+    js.timers.setTimeout(it.next()) {
+      for (i <- 1 to 5) {
+        for (j <- 1 to 4) {
+          graph.addNode(NodeD3(s"new-$i-$j", s"the new Node $i$j", i, s"<p>tooltip $i;$j</p>", s"node-$i-$j", if (i % 2 == 0) "online" else "offline", shape(i)))
+          graph.addLink(LinkD3Json(s"new-$i-$j", s"new-$i--", randomColor(j), j, endArrow = true))
+        }
+      }
+    }
+
     def randomColor(i: Int) = {
       val arrayColor = Array("#a6a6a6", "#9ff44d", "#ff84ff")
       arrayColor(Random.nextInt(arrayColor.length))
     }
+
+
 
     def shape(i: Int): D3Graph.Shape = i match {
       case t if t % 3 == 0 => D3Graph.Rect
